7928bfdb0069719787370f16199bdc90c08e0d9c
pacifiquerubasha <p.kishinyambwe@alustudent.com>
2024-05-04 12:00:02 +0400

    Initial setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+const Home:React.FC<{}> = () => {
+
+    return (
+        <Box>
+            HELLO WORLD
+        </Box>
+    );
+};
+
+export default Home;
